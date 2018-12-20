02d68e1aaa8c41bfcb74dfdaf8c8de45973272de
CptDoraemon <yxx0827@gmail.com>
2018-12-20 05:16:49 -0500

    update

--- src/components/card.js
+++ src/components/card.js
@@ -3,11 +3,72 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './card.css';
 
+import { GoArrowUp, GoArrowDown } from "react-icons/go";
+import { MdComment, MdShare, MdBookmark, MdHighlightOff, MdFlag } from "react-icons/md";
+
 
 class Card extends React.Component {
     render() {
         return (
             <div className='card-wrapper'>
+                <div className='card-sidebar'>
+                    <div className='card-sidebar-voteup'>
+                        <GoArrowUp size='25px'/>
+                    </div>
+                    <div className='card-sidebar-count'>
+                        6
+                    </div>
+                    <div className='card-sidebar-votedown'>
+                        <GoArrowDown size='25px'/>
+                    </div>
+
+                </div>
+                <div className='card-body'>
+                    <div className='card-body-info'>
+                        <span>Posted byu/vvvaduki 4 hours ago</span>
+                    </div>
+                    <div className='card-body-title'>
+                        <h3>来了又来了看来猪瘟闹的还不够大 俄动植物卫生监督局：中方允许进口俄冻禽肉</h3>
+                    </div>
+                    <div className='card-body-content'>
+                        <p>俄罗斯卫星通讯社莫斯科12月17日电 俄罗斯联邦动植物卫生监督局发布消息，中国检验检疫部门放行俄罗斯冻禽肉的进口。
+
+
+
+                            消息中说，中国检验检疫主管部门允许进口俄罗斯冻禽肉，被俄方列入审查合格名单之列的俄禽肉生产企业可以向中国出口冻禽肉。
+
+                            俄方在中国检验检疫部门在其官方网站正式公布这一名单后将启动对华出口冻禽肉。
+                            俄罗斯卫星通讯社莫斯科12月17日电 俄罗斯联邦动植物卫生监督局发布消息，中国检验检疫部门放行俄罗斯冻禽肉的进口。
+
+
+
+                            消息中说，中国检验检疫主管部门允许进口俄罗斯冻禽肉，被俄方列入审查合格名单之列的俄禽肉生产企业可以向中国出口冻禽肉。
+
+                            俄方在中国检验检疫部门在其官方网站正式公布这一名单后将启动对华出口冻禽肉。</p>
+                    </div>
+                    <div className='card-body-bottombar'>
+                        <div className='card-body-bottombar-item'>
+                            <MdComment size='20px'/>
+                            <span>comments</span>
+                        </div>
+                        <div className='card-body-bottombar-item'>
+                            <MdShare size='20px'/>
+                            <span>share</span>
+                        </div>
+                        <div className='card-body-bottombar-item'>
+                            <MdBookmark size='20px'/>
+                            <span>save</span>
+                        </div>
+                        <div className='card-body-bottombar-item'>
+                            <MdHighlightOff size='20px'/>
+                            <span>hide</span>
+                        </div>
+                        <div className='card-body-bottombar-item'>
+                            <MdFlag size='20px'/>
+                            <span>report</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
--- src/components/info.js
+++ src/components/info.js
@@ -38,29 +38,26 @@ class Info extends React.Component {
                     <div className='info-header' style={{backgroundColor:this.props.themeColor[1]}}>
                         <h5>guild officers</h5>
                     </div>
-                    <div className='info-body'>
-                        <div className='info-banner'>
-                            {this.props.themeLogo('30px')}
-                            <span> {this.props.themeTitle} </span>
+                    <div className='info-body flex-column-center'>
+                        <div className='info-officer'>
+                            <span className='info-officer-name' style={{color: this.props.themeColor[0]}}>u/placehoder</span>
+                            <span className='info-officer-signature'>signature placeholder</span>
                         </div>
-                        <div className='info-stats flex-row-even'>
-                            <div className='info-stat'>
-                                <p>0.0k</p>
-                                <h5>Guild Members</h5>
-                            </div>
-                            <div className='info-stat'>
-                                <p>000</p>
-                                <h5>Online</h5>
-                            </div>
+                        <div className='info-officer'>
+                            <span className='info-officer-name' style={{color: this.props.themeColor[0]}}>u/placehoder</span>
+                            <span className='info-officer-signature'>signature placeholder</span>
                         </div>
-                        <div className='info-message'>
-                            <p>泥潭被封，外野莫谈国事，这鸵鸟装不起啊，开个避难所好了 To English readers: This is a subreddit created as a fork of the famous Chinese forum, Saraba1st (or S1 in short)</p>
+                        <div className='info-officer'>
+                            <span className='info-officer-name' style={{color: this.props.themeColor[0]}}>u/placehoder</span>
+                            <span className='info-officer-signature'>signature placeholder</span>
                         </div>
-                        <button className='info-button' style={{backgroundColor: this.props.themeColor[0]}}>JOIN GUILD</button>
-                        <button className='info-button' style={{backgroundColor: this.props.themeColor[0]}}>create post</button>
                     </div>
                 </div>
                 <div className='info-wrapper'>
+                    <div className='info-about'>
+                        <p>This website is a web development practice project for my personal use.</p>
+                        <a href='https://www.xiaoxihome.com' target='_blank' rel='noopener noreferrer' style={{color: this.props.themeColor[0]}}>Author</a>
+                    </div>
                 </div>
             </div>
         )
